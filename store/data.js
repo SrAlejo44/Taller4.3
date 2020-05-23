78517416002093addc7703b54185d374684bddf2
SrAlejo44 <32855603+SrAlejo44@users.noreply.github.com>
2020-05-22 22:18:42 -0500

    Update data.js

--- store/data.js
+++ store/data.js
@@ -1,37 +1,37 @@
 // A collection of musicians where the key is the unique id
 module.exports = {
   coltrane: {
-    firstName: 'Web1',
+    firstName: 'Web2',
     lastName: 'Coltrane',
     genre: 'JAZZ',
   },
 
   davis: {
-    firstName: 'Web1',
+    firstName: 'Web2',
     lastName: 'Ejstes',
     genre: 'Rock',
   },
 
   mccartney: {
-    firstName: 'Web1',
+    firstName: 'Web2',
     lastName: 'McCartney',
     genre: 'ROCK',
   },
 
   hendrix: {
-    firstName: 'Web1',
+    firstName: 'Web2',
     lastName: 'Hendrix',
     genre: 'Rock',
   },
 
   cobain: {
-    firstName: 'Web1',
+    firstName: 'Web2',
     lastName: 'Cobain',
     genre: 'Rock',
   },
 
   king: {
-    firstName: 'Web1',
+    firstName: 'Web2',
     lastName: 'Hansson',
     genre: 'Rock',
   },
